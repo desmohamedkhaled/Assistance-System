f37e635d6c9ae4bdb007c9789bf815e0dba93af2
Mohamed Khaled <des.mohamedkhaled@gmail.com>
2025-09-15 10:00:41 +0300

    Fix MIME type error: Simplify vercel.json and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -26,14 +26,6 @@ export default defineConfig({
         manualChunks: {
           vendor: ['react', 'react-dom'],
           charts: ['chart.js', 'react-chartjs-2']
-        },
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          if (/\.(css)$/.test(assetInfo.name)) {
-            return `assets/css/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
         }
       }
     }
